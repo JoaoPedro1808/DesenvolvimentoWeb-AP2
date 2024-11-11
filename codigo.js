6fcaa815a1ce50d9efd7be2c14644a1edde6ce53
LE Monoplayer <jpedrossantana@gmail.com>
2024-11-11 17:16:07 -0300

    estilo da pagina de login

--- codigo.js
+++ codigo.js
@@ -34,7 +34,6 @@ const montacard = (atleta) =>{
     const cartao = document.createElement("article");
     const nome = document.createElement("h1");
     const imagem = document.createElement("img");
-    const descri = document.createElement("p");
 
     nome.innerHTML = atleta.nome;
     nome.style.fontFamily = 'sans-serif'
@@ -42,9 +41,6 @@ const montacard = (atleta) =>{
 
     imagem.src = atleta.imagem;
     cartao.appendChild(imagem);
-
-    descri.innerHTML = atleta.detalhes
-    cartao.appendChild(descri);
 
     cartao.onclick = manipulaClick;
     cartao.dataset.id = atleta.id;
